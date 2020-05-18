8532752f41027129379cac1124d6614c20de2f88
Drakoxw <dragon12xw@gmail.com>
2020-05-18 16:14:53 -0500

    primera ruta/

--- api/index.js
+++ api/index.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose')
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//const Users = mongoose.model('User', new mongoose.Schema({ name: String}))//!er modelo de prueba
+//Users.create({ name: 'Drako Super-admin'})//se le da nombre al modelo de prueba
+const router = express.Router()
 
-const Users = mongoose.model('User', new mongoose.Schema({ name: String}))//!er modelo de prueba
-Users.create({ name: 'Drako admin'})//se le da nombre al modelo de prueba
+//app.get('*', (req, res) => {
+    //Users.find()//busca todos lo usuarios
+      //  .then(x => res.send(x))//se llama a res y se le paso los usuarios})
 
-app.get('*', (req, res) => {
-    Users.find()//busca todos lo usuarios
-        .then(x => res.send(x))//se llama a res y se le paso los usuarios
+router.get('/', (req, res) => {
+    res.send('Oe desde ROOT')
 })
+app.use('/api/platos', router)
 
 module.exports = app
