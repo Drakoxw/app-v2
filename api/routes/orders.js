ddba8480db2d5b43b58fdad2ea172db0642cda74
Drakoxw <dragon12xw@gmail.com>
2020-05-18 18:36:51 -0500

    agregado mongodb a endpoints

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,26 +1,32 @@
 const express = require('express')
+const Orders = require('../models/Orders')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hola desde ordenes')//get para listar
+router.get('/', (req, res) => {//get para listar
+    Orders.find()
+    .exec()
+    .then(x => res.status(200).send(x)) 
 })
 
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)//get de elemento único
+router.get('/:id', (req, res) => {//get de elemento único
+    Orders.findById(req.params.id)
+        .exec()
+        .then(x => res.status(200).send(x))
 })
 
-router.post('/',(req, res) => {
-    res.send('soy post!')//ruta para crear
+router.post('/',(req, res) => {//ruta para crear
+    Orders.create(req.body)
+        .then(x => res.status(201).send(x))
 })
 
-router.put('/:id', (req, res) => {
-    res.send('Soy un put!')
-//router.put =+ metodo para reemplazar
+router.put('/:id', (req, res) => {//router.put =+ metodo para reemplazar/actualizar
+    Orders.findByIdAndUpdate(req.body.id, req.body)
+        .then(x => res.status(204).send(x))
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('Soy un delete')
+    Orders.findOneAndDelete(req.params.id).exec().then(() => res.sendStatus(204))
 })
 
 module.exports = router
--- api/routes/plates.js
+++ api/routes/plates.js
@@ -1,26 +1,32 @@
 const express = require('express')
+const Plates = require('../models/Meals')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Oe desde platos')
+router.get('/', (req, res) => {//get para listar
+    Plates.find()//buscar todo
+    .exec()// ejecuta la query para q devuelva una promesa con arreglo de los documentos q encontro
+    .then(x => res.status(200).send(x))// x son todos los elementos
 })
 
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)//get de elemento único
+router.get('/:id', (req, res) => {//get de elemento único
+    Plates.findById(req.params.id)
+        .exec()
+        .then(x => res.status(200).send(x))//
 })
 
-router.post('/',(req, res) => {
-    res.send('soy post!')//ruta para crear
+router.post('/',(req, res) => {//ruta para crear elemento
+    Plates.create(req.body)
+        .then(x => res.status(201).send(x))//status 201 creado
 })
 
-router.put('/:id', (req, res) => {
-    res.send('Soy un put!')
-//router.put =+ metodo para reemplazar
+router.put('/:id', (req, res) => {//router.put =+ metodo para reemplazar/actualizar
+    Plates.findByIdAndUpdate(req.body.id, req.body)// el 1er req busca por id y actualiza, el 2do es la actualizacion q hace
+        .then(x => res.status(204).send(x))//status 204 significa q no esta devolviendo ningun contenido
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('Soy un delete')
+    Plates.findOneAndDelete(req.params.id).exec().then(() => res.sendStatus(204))
 })
 
 module.exports = router
