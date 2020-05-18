d4c62530aae7c7345bbd857c0b226d86e6d7d1de
Drakoxw <dragon12xw@gmail.com>
2020-05-18 17:12:02 -0500

    refact con metodos get,put,delete y post

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,7 +3,24 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Hola desde ordenes')
+    res.send('Hola desde ordenes')//get para listar
+})
+
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)//get de elemento único
+})
+
+router.post('/',(req, res) => {
+    res.send('soy post!')//ruta para crear
+})
+
+router.put('/:id', (req, res) => {
+    res.send('Soy un put!')
+//router.put =+ metodo para reemplazar
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('Soy un delete')
 })
 
 module.exports = router
