9abb5268a2f463555928864f8ebed70e40bb4a62
unknown <dragon12xw@gmail.com>
2020-05-25 03:56:45 -0500

    0f borando cardando

--- web/main.js
+++ web/main.js
@@ -1,11 +1,24 @@
+const stringToHTML = (s) => {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(s, 'text/html')
+
+    return doc.body.firstChild
+}
+
+const renderItem = (item) => {
+    const element = stringToHTML(`<li data-id="${item._id}">${item.name}</li>`)
+    return element
+}
+
 window.onload = () => {
     fetch('https://v3ra.drakoxw.now.sh/api/platos')
         .then(response => response.json())
         .then(data => {
             const platosList = document.getElementById('list-platos')
             const submit = document.getElementById('submit')
-            const template = data.map(x => '<li>' + x.name + '</li>').join('')
-            platosList.innerHTML = template
+            const listItems = data.map(renderItem)
+            platosList.removeChild(platosList.firstElementChild)
+            listItems.forEach(element => platosList.appendChild(element))
             submit.removeAttribute('disabled')
         })
 }
