184988e83b7ce13e06d5123ca1a96b1dbad2e61f
unknown <dragon12xw@gmail.com>
2020-05-25 03:18:52 -0500

    0f boton desabilitado

--- web/main.js
+++ web/main.js
@@ -3,7 +3,9 @@ window.onload = () => {
         .then(response => response.json())
         .then(data => {
             const platosList = document.getElementById('list-platos')
+            const submit = document.getElementById('submit')
             const template = data.map(x => '<li>' + x.name + '</li>').join('')
             platosList.innerHTML = template
+            submit.removeAttribute('disabled')
         })
 }
