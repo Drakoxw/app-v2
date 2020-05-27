0d0c383bd28dff4391a296055a217f86be84f274
unknown <dragon12xw@gmail.com>
2020-05-27 07:55:50 -0500

    0f mostrando ordenes desde el servidor

--- web/main.js
+++ web/main.js
@@ -1,3 +1,4 @@
+
 const stringToHTML = (s) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(s, 'text/html')
@@ -15,6 +16,13 @@ const renderItem = (item) => {
         const platosIdInput = document.getElementById('plato-id')
         platosIdInput.value = item._id
     })
+
+    return element
+}
+
+const renderOrder = (order, plato) => {
+    const plat = plato.find(x => x._id === order.meal_id)
+    const element = stringToHTML(`<li data-id="${order._id}">${plat.name} - ${order.user_id} </li>`)
 
     return element
 }
@@ -37,13 +45,13 @@ window.onload = () => {
         fetch('https://v3ra.drakoxw.now.sh/api/ordenes',{
             method: 'POST',
             headers: {
-                'Content-Type': 'aplication/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(order)
         }).then(x => console.log(x))
     }
 
-    fetch('https://v3ra.drakoxw.now.sh/api/platos')
+    fetch('https://v3ra.drakoxw.now.sh/api/platos',)
         .then(response => response.json())
         .then(data => {
             const platosList = document.getElementById('list-platos')
@@ -52,5 +60,15 @@ window.onload = () => {
             platosList.removeChild(platosList.firstElementChild)
             listItems.forEach(element => platosList.appendChild(element))
             submit.removeAttribute('disabled')
+            fetch('https://v3ra.drakoxw.now.sh/api/ordenes')
+                .then(response => response.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('list-ordenes')
+                    const listOrders = ordersData.map(ordersData => renderOrder(ordersData,data))
+                    
+                    ordersList.removeChild(ordersList.firstElementChild)
+                    listOrders.forEach(element => ordersList.appendChild(element))
+                    
+                }) 
         })
 }
