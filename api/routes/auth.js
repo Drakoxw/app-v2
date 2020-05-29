9a5db1dcf017cb3a929970f66c85a838a5fe5f0e
unknown <dragon12xw@gmail.com>
2020-05-28 20:41:05 -0500

    0f ruta de registro creada

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const crypto = require('crypto')
 const Users = require('../models/Users')
 
 const router = express.Router()
 
 router.post('/register',(req, res) => {
-    res.send('form de reistro')
+    const { email, password } = req.body
+    crypto.randomBytes(16, (err, salt) => {
+        const newSalt = salt.toString('base64')
+        crypto.pbkdf2(password, newSalt, 1000, 64, 'sha1', (err, key) => {
+            const encryptedPass = key.toString('base64')
+            Users.findOne({ email }).exec()
+                .then(user => {
+                    if (user) {
+                        return res.send('Usuario ya existe!')
+                    }
+                    Users.create({
+                        email,
+                        password: encryptedPass,
+                        salt: newSalt,
+                    }).then(() => {
+                        res.send('Usuario creado!')
+                    })
+                })
+        })
+    })
 })
 
 router.post('/login',(req, res) => {
